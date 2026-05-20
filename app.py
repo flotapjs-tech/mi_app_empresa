ba0e41c5c7dff27a305c2087cfd8c5c2045844af
Facundo Rey <flotapjs@gmail.com>
2026-05-20 14:00:39 -0300

    arreglo left join en asig_condcutor

--- app.py
+++ app.py
@@ -1887,8 +1887,8 @@ def infracciones_conductor(conductor_id):
                 i.numero,
                 i.vehiculo_id
             FROM infracciones i
-            JOIN vehiculos v
-                ON i.vehiculo_id = 2
+            LEFT JOIN vehiculos v
+                ON i.vehiculo_id = v.id
             WHERE i.conductor_id IS NULL
             ORDER BY i.fecha DESC, i.hora DESC
         """)
@@ -1913,7 +1913,7 @@ def infracciones_conductor(conductor_id):
                 i.numero,
                 i.vehiculo_id
             FROM infracciones i
-            JOIN vehiculos v
+            LEFT JOIN vehiculos v
                 ON i.vehiculo_id = v.id
             WHERE i.conductor_id = %s
             ORDER BY i.fecha DESC, i.hora DESC
